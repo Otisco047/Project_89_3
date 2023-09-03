385f6263df6cf7facb51b7372640b280584c34de
Otisco047 <133856261+Otisco047@users.noreply.github.com>
2023-09-03 19:10:40 +1200

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+player1_name = localStorage.getItem("p1key");
+player2_name = localStorage.getItem("p2key");
+document.getElementById("player1_name").innerHTML = player1_name;
+document.getElementById("player2_name").innerHTML = player2_name;
+document.getElementById("q_turn").innerHTML = "Question turn: " + player1_name;
+document.getElementById("a_turn").innerHTML = "Answer turn" + player2_name;
+player1_score = 0;
+player2_score = 0;
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
